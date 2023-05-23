927d6779d7f26eda44b9c7ded351115f4ea0c2ea
Alex <fuchs222@gmail.com>
2023-05-23 07:39:01 +0200

    import settings with json

---
+++ config1.py
@@ -0,0 +1,15 @@
+server = {
+    "vm0_old": ["lnx8934.st.zkb.ch"],
+    "vm1_old": ["lnx8843.st.zkb.ch"],
+    "vm0_new": ["lnx9812.st.zkb.ch"],
+    "vm1_new": ["lnx9563.st.zkb.ch"],
+    "host_list_old": ["lnx8934.st.zkb.ch", "lnx8843.st.zkb.ch"],
+    "host_list_new": ["lnx9812.st.zkb.ch", "{lnx9563.st.zkb.ch"],
+}
+
+app = {
+    "appid": "1234",
+    "apptc": "slbriskreporting",
+    "appweb": "slbriskreportingweb",
+    "av": "hans muster",
+}
